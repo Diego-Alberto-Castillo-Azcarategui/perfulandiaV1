92a0c859ebb9a5306d532a325343f753e00b4c20
Diego Azcarategui <dia.castilloa@duocuc.cl>
2025-05-06 17:29:39 -0400

    Se reestructura según lo solicitado en rubrica, se crea UsuarioController, UsuarioServicio, UsuarioServicioImpl se le añade "Impl" como buena practica para especificar que una clase implementa una interfaz llevan en si la logica de negocio de usuarios.

---
+++ src/main/java/cl/duoc/perfulandia/controller/UsuarioController.java
@@ -0,0 +1,65 @@
+package cl.duoc.perfulandia.controller;
+
+import cl.duoc.perfulandia.dto.MessageResponse;
+import cl.duoc.perfulandia.model.Usuario;
+import cl.duoc.perfulandia.service.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obtenerUsuarios() {
+        return ResponseEntity.ok(usuarioServicio.obtenerUsuarios());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable String id) {
+        Usuario encontrado = usuarioServicio.getUsuario(id);
+        if (encontrado != null) {
+            return ResponseEntity.ok(encontrado);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<MessageResponse> crearUsuario(@RequestBody Usuario request) {
+        boolean agregado = usuarioServicio.agregarUsuario(request);
+        if (!agregado) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new MessageResponse("Error: Usuario ya existente."));
+        }
+        return ResponseEntity.ok(new MessageResponse("Usuario creado con éxito."));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MessageResponse> reemplazarUsuario(@PathVariable String id,
+                                                             @RequestBody Usuario request) {
+        boolean reemplazado = usuarioServicio.reemplazarUsuario(id, request);
+        if (!reemplazado) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new MessageResponse("Usuario fue reemplazado con éxito."));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MessageResponse> eliminarUsuario(@PathVariable String id) {
+        boolean eliminado = usuarioServicio.eliminarUsuario(id);
+        if (!eliminado) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new MessageResponse("Usuario eliminado con éxito."));
+    }
+}
+
