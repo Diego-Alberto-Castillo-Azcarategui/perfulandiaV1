1b7fcd192a1f48855486f1f2bcc71ad326debdea
Bastian Ahumada <b.ahumadaa@duocuc.cl>
2025-05-25 13:39:19 -0400

    Se crea repositorio de Producto

--- src/main/java/cl/duoc/perfulandia/repository/ProductoRepositorio.java
+++ src/main/java/cl/duoc/perfulandia/repository/ProductoRepositorio.java
@@ -1,8 +1,10 @@
 package cl.duoc.perfulandia.repository;
 
 import cl.duoc.perfulandia.service.dominio.Producto;
+import cl.duoc.perfulandia.service.dominio.Usuario;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,11 +23,57 @@ public class ProductoRepositorio {
                                 .descripcion("Perfume con un delicioso olor a vainilla")
                                 .precio("30.000")
                                 .categoria("Perfumes")
+                                .build(),
+                        Producto.builder()
+                                .id("POO2")
+                                .nombre("Perfume Ben 10")
+                                .descripcion("Perfume con olor a limon")
+                                .precio("10.000")
+                                .categoria("Perfumes")
+                                .build(),
+                        Producto.builder()
+                                .id("P003")
+                                .nombre("Essence Mystique")
+                                .descripcion("Perfume floral con notas de jazmín y vainilla")
+                                .precio("59.900")
+                                .categoria("Perfumes")
+                                .build(),
+                        Producto.builder()
+                                .id("P004")
+                                .nombre("Ocean Breeze")
+                                .descripcion("Fragancia fresca con notas cítricas y acuáticas")
+                                .precio("49.750")
+                                .categoria("Perfumes")
+                                .build(),
+                        Producto.builder()
+                                .id("P009")
+                                .nombre("Noir Élégance")
+                                .descripcion("Perfume oriental con notas de ámbar y madera de sándalo, para la noche")
+                                .precio("68.400")
+                                .categoria("Perfumes")
                                 .build()
 
                 )
         );
     }
+    public static List<Producto> findAll() {return productos;}
 
+    public static Producto findById(String id) {
+        for (Producto producto : productos) {
+            if (producto.getId().equals(id)) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    public static Producto agregarProducto(Producto producto) {productos.add(producto);}
+
+    public static void reemplazarProducto(Producto toReplace, Producto request) {
+        int index = productos.indexOf(toReplace);
+        productos.set(index,request);
+    }
+
+    public static void eliminarProducto(Producto encontrado){ productos.remove(encontrado);}
 
 }
